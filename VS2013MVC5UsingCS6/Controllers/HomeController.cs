8bd3ab997fb566fc707744369dd14edfa3e11a17
Will <doggy.huang@gmail.com>
2015-07-29 14:13:09 +0800

    加入 C# 6.0 語法 (

--- VS2013MVC5UsingCS6/Controllers/HomeController.cs
+++ VS2013MVC5UsingCS6/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace VS2013MVC5UsingCS6.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = $"Your application description page.";
 
             return View();
         }
