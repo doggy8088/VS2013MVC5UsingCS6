9c146b871a6130f9b1bec8dee09d558a62a8bc64
Will <doggy.huang@gmail.com>
2015-07-29 13:05:16 +0800

    Initial commit for ASP.NET MVC 5.2.2 using Visual Studio 2013 ( C# 5 )

---
+++ VS2013MVC5UsingCS6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VS2013MVC5UsingCS6.MvcApplication" Language="C#" %>
---
+++ VS2013MVC5UsingCS6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS2013MVC5UsingCS6.Startup))]
+namespace VS2013MVC5UsingCS6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
